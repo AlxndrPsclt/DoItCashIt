4f68ae6ce35888debdb83c385564da58c4c83c48
Alex <tai_04@laposte.net>
2013-05-19 00:42:37 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoItCashIt::Application.config.secret_token = 'c00f59dd97472146dd61a11759323d9184495263addf21c09773fbddbf9d583ddc4920e1c6a06878da30a33c1db874cec3ceb1316452d5c6ca2e8a31de0fcf22'
