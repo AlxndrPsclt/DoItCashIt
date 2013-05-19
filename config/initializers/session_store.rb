130b0a12594b3ac11035a0c57cdac0e0457739a5
Fabrice GAUDIN <fabrice_gaudin@hotmail.fr>
2013-05-19 16:44:53 +0200

    Add a StaticPages controller

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-DoItCashIt::Application.config.session_store :cookie_store, key: '_DoItCashIt_session'
+DoItCashIt::Application.config.session_store :cookie_store, :key => '_DoItCashIt_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
