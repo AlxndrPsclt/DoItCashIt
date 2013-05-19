7aeedec698e7b3c1a6b61d66691e140fa863b047
Andrin Gabriel <gabriel.andrin@telecom-bretagne.eu>
2013-05-19 17:29:29 +0200

    Third commit by AirKiller
    test et User model fait jusqu'a la partie password

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,9 @@
 
 class User < ActiveRecord::Base
 	attr_accessible :dateDerniereConnection, :email, :jetons, :jetonsTotalDepuisInscription, :name, :password_digest
-
-	validates :name, presence: true
-	validates :email, presence: true
+	before_save { |user| user.email = email.downcase }
+	
+	validates :name,  presence: true,length: { maximum: 25 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
