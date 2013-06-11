96b8251e8f663734a79f16219b1c98915740ccd3
Andrin <gabriel.andrin@telecom-bretagne.fr>
2013-06-11 18:19:54 +0200

    avant dernier commit?

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,4 +7,16 @@ module UsersHelper
 		image_tag(gravatar_url, alt: user.name, class: "gravatar")
 	end
 
+	def gravatar_for_comment(user)
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=25"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end
+
+	def gravatar_for_header(user)
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=175"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end
+
 end
